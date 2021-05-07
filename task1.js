06f721eb5ea186795bf215dab91ce59e24397f9a
Alisa Morenko <Alisa.Morenko@gmail.com>
2021-05-08 00:00:34 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -3,9 +3,12 @@
 // Значением исполнившегося промиса должно быть то кол - во миллисекунд которое передали во время вызова функции delay.
 
 const delay = ms => {
-  // Твой код
+  return new Promise(resolve =>
+    setTimeout(() => {
+      resolve(ms);
+    }, ms),
+  );
 };
-
 const logger = time => console.log(`Resolved after ${time}ms`);
 
 // Вызовы функции для проверки
